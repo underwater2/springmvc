37a42ac960442becd6188869db55f8869fdd2841
underwater2 <shaian2@naver.com>
2023-09-07 16:54:35 +0900

    6.5 HTTP 요청 - 기본, 헤더 조회까지

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,30 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController //리턴값이 뷰의 이름이 아니고, 메세지 바디에 들어가서 반환된다.
+public class LogTestController {
+
+    //@Slf4j 넣으면 자동으로 log 만들어진다.
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+}
